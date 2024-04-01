b8f33154cdfd75685cd7449ac2101cb907551e2c
souravasnoda <62390888+souravasnoda@users.noreply.github.com>
2024-04-01 21:14:43 +0530

    Add files via upload

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import EmployeeService from "../services/EmployeeService";
+import Employee from "./Employee";
+
+const EmployeeList = () => {
+  const navigate = useNavigate();
+  const [employees, setEmployees] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await EmployeeService.getEmployees();
+        setEmployees(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const deleteEmployee = (e,id)=>{
+    e.preventDefault();
+    EmployeeService.deleteEmployee(id)
+    .then((res)=>{
+        if(employees){
+            setEmployees((prevElement)=>{
+                return prevElement.filter((employee)=>employee.id!==id);
+            });
+        }
+        
+    });
+
+  }
+
+  return (
+    <div className="container mx-auto my-8">
+      <div className="h-12 ">
+        <button
+          onClick={() => navigate(`/addEmployee`)}
+          className="rounded bg-slate-600 text-white px-6 py-2 font-semibold"
+        >
+          Add Employee
+        </button>
+      </div>
+      <div className="flex shadow border-b">
+        <table className="min-w-full">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                First Name
+              </th>
+              <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                Last Name
+              </th>
+              <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                Email
+              </th>
+              <th className="text-right font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                Actions
+              </th>
+            </tr>
+          </thead>
+
+          {/* make sure every thing is loaded */}
+
+          {!loading && employees&&(
+            <tbody className="bg-white">
+              {employees.map((employee) => (
+                <Employee employee={employee} deleteEmployee={deleteEmployee} key={employee.id}/>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeList;
